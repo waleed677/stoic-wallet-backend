f91567713d316d8f788369ea0c6bf4d2da24cef0
waleed677 <waleedmalik677@gmail.com>
2023-03-21 04:10:43 +0500

    add netlify

---
+++ src/api.js
@@ -0,0 +1,38 @@
+const Shopify = require('shopify-api-node');
+const express = require('express');
+const cors = require('cors');
+const serverless = require("serverless-http");
+
+const route = express.Router();
+
+const app = express();
+const port = process.env.PORT || 4000;
+app.use(cors());
+
+
+const shopify = new Shopify({
+    shopName: 'is-not-art',
+    accessToken: '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',
+  });
+
+  // Define an API endpoint to fetch all price rules for the store
+
+route.get('/price-rules', async (req, res) => {
+    try {
+      // Fetch all price rules for the store using the Shopify API
+      const priceRules = await shopify.priceRule.list();
+      res.send(priceRules);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error fetching price rules');
+    }
+  });
+  
+  // Start the server
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+
+  app.use('/.netlify/functions/api',route)
+
+  module.exports.handler = serverless(app)
